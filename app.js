954557b90f6be149756c24ea833d250157313374
Beaker8314 <49918132+Beaker8314@users.noreply.github.com>
2022-04-01 00:38:19 -0400

    updated googlestrategy callback url

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets2",
+    callbackURL: "https://ancient-shore-89005.herokuapp.com/",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
